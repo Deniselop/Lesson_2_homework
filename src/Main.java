521f1553675b74407582945d0278d27a26046d34
Deniselop <volejkodenis302@gmail.com>
2022-11-24 19:08:30 +0300

    add new commit

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,14 @@ import static java.lang.System.in;
 import static java.lang.System.setOut;
 
 public class Main {
+    static String[] someExternalValue;
 
     //sout
     //psvm
 
    public  static void main(String[] args) {
+
+
 
 
        for (int i = 0; i < 10; i++) {
@@ -20,22 +23,31 @@ public class Main {
 
        }
 
-       for (int b = 0; b < 10; ) {
-           Scanner input = new Scanner(System.in);
 
-           System.out.print("Введите имя пользователя:");
-           char letter = input.next().charAt(0);
+
+       for (int b = 0; b < 10; b++ )
+
+           {
+               Scanner input = new Scanner(in);
+
+               System.out.print("Введите имя пользователя:");
+
+
+               char letter = input.next().charAt(0);
 
            if (Character.toString(letter).matches("^[а-яА-Я]*$")) {
+
                System.out.println("Здравствуйте, " + letter + "!");
            } else {
                System.out.println("Пожалуйста, вводите только буквы!");
            }
-           b++;
 
-           return;
 
-       }
+
+           }
+
    }
+
+
 
 }
